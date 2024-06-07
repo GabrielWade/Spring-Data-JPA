73546ec91453475f3baf7e08f390fd248ddcdff5
Gabriel Lima <gablima004@gmail.com>
2024-06-06 22:00:52 -0300

    atualizando dependencias web, e rodando a api no servidor

---
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -0,0 +1,12 @@
+package br.com.alura.screenmatch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SerieController {
+    @GetMapping("/series")
+    public String obterSeries() {
+        return "Hello, World!";
+    }
+}
